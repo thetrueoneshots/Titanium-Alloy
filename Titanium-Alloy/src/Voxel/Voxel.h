fe9bc53d8e0f72aa125d51e7ddc0df8183c7e9a1
= <gngroenewegen@gmail.com>
2020-12-31 17:31:17 +0100

    Update voxel.h documentation

--- Titanium-Alloy/src/Voxel/Voxel.h
+++ Titanium-Alloy/src/Voxel/Voxel.h
@@ -4,6 +4,8 @@
 * Voxel is a framework created for rendering voxels. Made by Gijs Groenewegen. 
 * Usage: #include "Voxel/Voxel.h"
 * For more control of the framework, extra classes can be included.
+* Reading the readme and the documentation in the files below should give you a good indication on how to
+* use the framework.
 */
 
 #include "src/Mesh/CubLoader.h"
